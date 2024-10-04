726a06d3cc65bb39a2059c00502d5aa78acbf1ee
Lygia Dias <lygia.sdias@muu.agr.br>
2024-10-03 23:57:52 -0300

    feat: create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,72 @@
-import Image from "next/image";
+// app/page.tsx
+"use client";
+
+import { Suspense, useState } from 'react';
+import Link from 'next/link';
+import SelectInput from '../components/SelectInput';
+import VehicleMakesLoader from '../components/VehicleMakesLoader';
+import Image from 'next/image'; 
+import developsLogo from '../public/images/develops-logo.png';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [selectedMakeId, setSelectedMakeId] = useState<string>('');
+  const [selectedYear, setSelectedYear] = useState<string>('');
+  const [vehicleMakeOptions, setVehicleMakeOptions] = useState<{ value: string; label: string; }[]>([]);
+  const currentYear = new Date().getFullYear();
+  const years = Array.from({ length: currentYear - 2015 + 1 }, (_, i) => currentYear - i);
+  
+  const yearOptions = years.map((year) => ({
+    value: year.toString(),
+    label: year.toString(),
+  }));
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const isNextEnabled = selectedMakeId && selectedYear;
+
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-white p-4"> 
+      
+   
+      <Image
+        src={developsLogo}
+        alt="Develops Logo"
+        width={500}
+        className="mb-4" 
+      />
+      <h1 className="text-2xl font-bold text-gray-600 mb-4">Develops Today Dealer App</h1>
+
+      <Suspense fallback={<h2 className="text-lg">Loading...</h2>}>
+        <VehicleMakesLoader setVehicleMakeOptions={setVehicleMakeOptions} />
+
+        <div className="w-full max-w-md space-y-4 justify-center mt-20">
+          <SelectInput
+            label="Vehicle Makes"
+            id="make"
+            options={vehicleMakeOptions} 
+            value={selectedMakeId} 
+            onChange={setSelectedMakeId} 
+            placeholder="Select a vehicle"
+          />
+
+          <SelectInput
+            label="Model Year"
+            id="year"
+            options={yearOptions}
+            value={selectedYear} 
+            onChange={setSelectedYear}
+            placeholder="Year"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      </Suspense>
+
+      <Link href={`/result?makeId=${selectedMakeId}&year=${selectedYear}`}>
+        <button
+          disabled={!isNextEnabled}
+          className={`mt-6 px-4 py-2 text-white font-semibold rounded-md transition-colors duration-300 
+          ${isNextEnabled ? 'bg-blue-600 hover:bg-blue-700' : 'bg-gray-400 cursor-not-allowed'}`}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Next
+        </button>
+      </Link>
     </div>
   );
 }
